dfa13af6003d6b661f25db61f5de3429139cb26a
song1900 <woojin1900@gmail.com>
2024-03-15 14:35:25 +0900

    Init Project

---
+++ GIL/GIL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GIL
+//
+//  Created by 송우진 on 3/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
