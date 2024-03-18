d46ceae0dfbb6dde6437ec4ee549b8e1bf85a07c
Woojin Song <woojin1900@gmail.com>
2024-03-18 14:56:39 +0900

    Feat: Main.Storyboard 제거 및 LoginViewController 생성 후 RootViewController로 등록

---
+++ GIL/GIL/ViewController/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  GIL
+//
+//  Created by 송우진 on 3/18/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
