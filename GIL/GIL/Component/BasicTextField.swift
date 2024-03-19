0dafdd2f88b2011ecc202258b126ac5f40d78061
Woojin Song <woojin1900@gmail.com>
2024-03-19 10:45:04 +0900

    Feat: 앱에서 사용할 가장 기본적인 UIButton, UITextField 생성

---
+++ GIL/GIL/Component/BasicTextField.swift
@@ -0,0 +1,73 @@
+//
+//  BasicTextField.swift
+//  GIL
+//
+//  Created by 송우진 on 3/18/24.
+//
+
+import UIKit
+
+class BasicTextField: UITextField {
+    enum FormType {
+        case email
+        case password
+        
+        var keyboardType: UIKeyboardType {
+            switch self {
+            case .email:
+                return .emailAddress
+            case .password:
+                return .default
+            }
+        }
+        
+        var placeholder: String {
+            switch self {
+            case .email:
+                return "Email"
+            case .password:
+                return "Password"
+            }
+        }
+    }
+    
+    private let padding: UIEdgeInsets = UIEdgeInsets(top: 8, left: 10, bottom: 8, right: 10)
+    private let formType: FormType
+    
+    init(type: FormType) {
+        self.formType = type
+        super.init(frame: .zero)
+        
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+}
+
+// MARK: - Configure UI
+extension BasicTextField {
+    func configureUI() {
+        keyboardType = formType.keyboardType
+        placeholder = formType.placeholder
+        textColor = .black
+        font = .systemFont(ofSize: 14, weight: .medium)
+        layer.borderColor = UIColor.lightGray.cgColor
+        layer.borderWidth = 1
+        layer.cornerRadius = 6
+        autocorrectionType = .no
+        autocapitalizationType = .none
+        isSecureTextEntry = formType == .password ? true : false
+        
+    }
+}
