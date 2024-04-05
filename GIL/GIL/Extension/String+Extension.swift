3d8847d66ef2262a2d27f4f5a6cde7462976f0bb
Woojin Song <woojin1900@gmail.com>
2024-04-05 23:44:15 +0700

    Refactor: `Regex` 타입을 사용하여 더 간결하고 읽기 쉽도록 리팩토링

--- GIL/GIL/Extension/String+Extension.swift
+++ GIL/GIL/Extension/String+Extension.swift
@@ -7,25 +7,37 @@
 
 import Foundation
 
+// MARK: - 정규식
+extension String {
+    // 이메일 형식
+    static let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+    // 영문자와 숫자만 포함
+    static let alphanumericRegex = "^[A-Za-z0-9]+$"
+    // 최소 하나의 대문자를 포함
+    static let uppercaseLetterRegex = ".*[A-Z]+.*"
+    // 최소 하나의 소문자를 포함
+    static let lowercaseLetterRegex = ".*[a-z]+.*"
+    // 최소 하나의 숫자를 포함
+    static let numberRegex = ".*[0-9]+.*"
+}
+
+// MARK: - 회원가입, 로그인에 필요한 유효성 검사
 extension String {
     /**
      주어진 문자열이 유효한 이메일 주소인지 검사
      - Returns: 이메일 주소가 유효한 경우 true, 그렇지 않은 경우 false를 반환
      */
     func isValidEmail() -> Bool {
-        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
-        
         do {
-            let regex = try NSRegularExpression(pattern: emailRegex)
-            let matches = regex.matches(in: self, range: NSRange(location: 0, length: self.utf16.count))
-            return !matches.isEmpty
+            let emailRegex = try Regex(String.emailRegex)
+            return self.firstMatch(of: emailRegex) != nil
         } catch {
             return false
         }
     }
     
     /**
-     비밀번호 유효성 검사 함수
+     비밀번호 유효성 검사
      - Returns: 비밀번호 유효성 검사 결과. 다음 조건을 모두 만족해야 `true`반환
      - 최소 10자 이상
      - 적어도 하나의 소문자를 포함
@@ -34,39 +46,41 @@ extension String {
      - 영문자와 숫자만을 포함
      */
     func isValidPassword() -> Bool {
-        // 최소 10자 이상
-        guard self.count >= 10 else {
+        do {
+            // 최소 10자 이상
+            guard self.count >= 10 else {
+                return false
+            }
+            
+            // 영문자와 숫자만 포함
+            let alphanumericRegex = try Regex(String.alphanumericRegex)
+            guard let _ = self.firstMatch(of: alphanumericRegex) else {
+                return false
+            }
+            
+            // 대문자 포함
+            let uppercaseLetterRegex = try Regex(String.uppercaseLetterRegex)
+            guard let _ = self.firstMatch(of: uppercaseLetterRegex) else {
+                return false
+            }
+            
+            // 소문자 포함
+            let lowercaseLetterRegex = try Regex(String.lowercaseLetterRegex)
+            guard let _ = self.firstMatch(of: lowercaseLetterRegex) else {
+                return false
+            }
+            
+            // 숫자 포함
+            let numberRegex = try Regex(String.numberRegex)
+            guard let _ = self.firstMatch(of: numberRegex) else {
+                return false
+            }
+            
+            return true
+
+        } catch {
             return false
         }
-
-        // 영문자와 숫자만 포함
-        let alphanumericRegex = "^[A-Za-z0-9]+$"
-        let alphanumericTest = NSPredicate(format:"SELF MATCHES %@", alphanumericRegex)
-        if !alphanumericTest.evaluate(with: self) {
-            return false
-        }
-
-        // 대문자 포함
-        let uppercaseLetterRegex = ".*[A-Z]+.*"
-        let uppercaseLetterTest = NSPredicate(format:"SELF MATCHES %@", uppercaseLetterRegex)
-        guard uppercaseLetterTest.evaluate(with: self) else {
-            return false
-        }
-
-        // 소문자 포함
-        let lowercaseLetterRegex = ".*[a-z]+.*"
-        let lowercaseLetterTest = NSPredicate(format:"SELF MATCHES %@", lowercaseLetterRegex)
-        guard lowercaseLetterTest.evaluate(with: self) else {
-            return false
-        }
-
-        // 숫자 포함
-        let numberRegex = ".*[0-9]+.*"
-        let numberTest = NSPredicate(format:"SELF MATCHES %@", numberRegex)
-        guard numberTest.evaluate(with: self) else {
-            return false
-        }
-
-        return true
     }
+    
 }
