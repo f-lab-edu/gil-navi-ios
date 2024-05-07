8666e3a5cde7e8cbde26b7097f50adde0b66db8e
Woojin Song <woojin1900@gmail.com>
2024-05-07 17:23:00 +0900

    Refactor: PlaceSearch 코드 리팩토링
    기능별로 파일로 분리시켜서 관리하던 방법에서 CollectionView를 관리 담당하는 PlaceSearchCollectionViewHandler를 생성했습니다.

--- GIL/GIL/Screen/PlaceSearch/PlaceSearchViewController.swift
+++ GIL/GIL/Screen/PlaceSearch/PlaceSearchViewController.swift
@@ -10,18 +10,15 @@ import CoreLocation
 import MapKit
 
 final class PlaceSearchViewController: BaseViewController, NavigationBarHideable {
-    enum Section: CaseIterable {
-        case main
-    }
-    
-    var viewModel: PlaceSearchViewModel
-    var placeSearchView = PlaceSearchView()
-    var dataSource: UICollectionViewDiffableDataSource<Section, Place>!
+    private var viewModel: PlaceSearchViewModel
+    private var placeSearchView = PlaceSearchView()
+    private var placeSearchCollectionViewHandler: PlaceSearchCollectionViewHandler?
     
     // MARK: - Initialization
     init(viewModel: PlaceSearchViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
+        placeSearchCollectionViewHandler = PlaceSearchCollectionViewHandler(viewModel: viewModel, placeSearchView: placeSearchView)
     }
     
     required init?(coder: NSCoder) {
@@ -36,8 +33,6 @@ final class PlaceSearchViewController: BaseViewController, NavigationBarHideable
     override func viewDidLoad() {
         super.viewDidLoad()
         setupBindings()
-        setupCollectionView()
-        configureDataSource()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -48,5 +43,66 @@ final class PlaceSearchViewController: BaseViewController, NavigationBarHideable
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         showNavigationBar(animated: false)
+    }
+}
+
+// MARK: - Actions
+extension PlaceSearchViewController {
+    private func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - Setup Binding
+extension PlaceSearchViewController {
+    private func setupBindings() {
+        bindButtons()
+        bindSearchBar()
+        bindLocationService()
+        bindSearchMapItems()
+    }
+    
+    private func bindButtons() {
+        placeSearchView.navigationBar.backButton.addAction( UIAction { _ in self.backButtonTapped()}, for: .touchUpInside)
+    }
+    
+    private func bindSearchBar() {
+        placeSearchView.navigationBar.searchBar.delegate = self
+    }
+    
+    private func bindLocationService() {
+        viewModel.locationService.delegate = self
+    }
+    
+    private func bindSearchMapItems() {
+        viewModel.$mapItems
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] mapItems in
+                guard let self else { return }
+                self.placeSearchCollectionViewHandler?.applySnapshot(with: mapItems)
+            }
+            .store(in: &viewModel.cancellables)
+    }
+}
+
+// MARK: - UISearchBarDelegate
+extension PlaceSearchViewController: UISearchBarDelegate {
+    func searchBar(
+        _ searchBar: UISearchBar,
+        textDidChange searchText: String
+    ) {
+        viewModel.searchPlace(searchText)
+    }
+}
+
+// MARK: - LocationServiceDelegate
+extension PlaceSearchViewController: LocationServiceDelegate {
+    func didFetchAddress(_ address: String) {
+        viewModel.locationService.stopUpdatingLocation()
+        placeSearchView.navigationBar.addressLabel.text = address
+    }
+    
+    func didFailWithError(_ error: Error) {
+        Log.error("LocationService Error: \(error.localizedDescription)", error)
     }
 }
