0ed407eebd84c09cae289466f29251f40ed7da4f
Woojin Song <woojin1900@gmail.com>
2024-05-16 16:01:13 +0900

    Chore: LoginViewUITests 코드 수정

--- GIL/GILUITests/LoginViewUITests.swift
+++ GIL/GILUITests/LoginViewUITests.swift
@@ -21,18 +21,12 @@ final class LoginViewUITests: XCTestCase {
         app.textFields["EmailTextField"].tap()
         // 텍스트 필드에 이메일 주소를 입력합니다.
         app.textFields["EmailTextField"].typeText("test@mail.com")
-        
         // 키보드에서 "next" 버튼을 탭하여 다음 입력 필드로 이동합니다.
         app.keyboards.buttons["next"].tap()
-        
-        // "PasswordTextField" 텍스트 필드를 찾아 탭합니다.
-        app.secureTextFields["PasswordTextField"].tap()
-        // 선택된 보안 텍스트 필드에 비밀번호를 입력합니다.
+        // "PasswordTextField" 보안 텍스트 필드에 비밀번호를 입력합니다.
         app.secureTextFields["PasswordTextField"].typeText("TestTest1900@")
-        
         // 키보드에서 "done" 버튼을 탭하여 키보드를 숨깁니다.
         app.keyboards.buttons["done"].tap()
-        
         // "SignInButton" 버튼을 찾아 로그인을 시도하기 위해 탭합니다.
         app.buttons["SignInButton"].tap()
     }
