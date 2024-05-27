5f585947a8ab9bb14158280b5a0d57a8fa2a13e2
Woojin Song <woojin1900@gmail.com>
2024-05-27 17:31:33 +0900

    Test: 로그인 화면 Snapshot Testing

--- GIL/GILTests/SnapshotTests/GILSnapshotTests.swift
+++ GIL/GILTests/SnapshotTests/GILSnapshotTests.swift
@@ -6,30 +6,18 @@
 //
 
 import XCTest
+import SnapshotTesting
+@testable import GIL
 
 final class GILSnapshotTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_로그인화면() {
+        let mockAuthManager = MockFirebaseAuthManager()
+        let dependencies = AuthenticationSceneDIContainer.Dependencies(firebaseAuthManager: mockAuthManager)
+        let diContainer = AuthenticationSceneDIContainer(dependencies: dependencies)
+        
+        let actions = LoginViewModelActions(showSignUp: {})
+        let loginViewController = diContainer.makeLoginViewController(actions: actions)
+        
+        assertSnapshot(matching: loginViewController, as: .image, named: "login_view_controller", testName: "ViewController")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
